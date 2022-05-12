0d834f3831dd5f552f723a1d475e91d9d49de29d
Skillz <gankstarsleague@gmail.com>
2022-05-12 16:50:52 -0400

    fix: dont leave open responses

--- packages/rest/index.ts
+++ packages/rest/index.ts
@@ -89,7 +89,7 @@ export class RestManager {
         Authorization: this.botToken,
         "X-Audit-Log-Reason": reason ? encodeURIComponent(reason) : "",
       },
-    });
+    }).then((response) => response.body?.cancel());
   }
 }
 
